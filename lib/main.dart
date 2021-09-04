7de3580d993f765f1ad5ff9aa8f97ab2a812ee81
Henry-Peti <iamgadgetboy@gmail.com>
2021-09-05 11:05:16 +1200

    layout rooms on ui

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:friendlychat/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'config/palette.dart';
+
+void main() {
+  runApp(new FriendlychatApp()); 
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp( 
+      title: "FriendlyChat",
+      debugShowCheckedModeBanner: false,
+      theme: 
+        ThemeData(
+          appBarTheme: const AppBarTheme(backgroundColor: Palette.background),
+          scaffoldBackgroundColor:Palette.background,
+          primaryColor: Colors.white,
+          accentColor: Palette.green,
+          iconTheme: const IconThemeData(color: Colors.black),
+          fontFamily: GoogleFonts.montserrat().fontFamily,
+          textTheme: GoogleFonts.montserratTextTheme(),
+        ),
+      home: new HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:friendlychat/widgets/room_card.dart';
+import 'package:friendlychat/widgets/upcoming_rooms_list.dart';
+import 'package:friendlychat/widgets/widgets.dart';
+import 'package:friendlychat/data.dart';
+
+
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            CupertinoIcons.search,
+            size: 30.0,
+          ),
+          onPressed: (){},
+        ),
+        actions: [
+            IconButton(
+          icon: const Icon(
+            CupertinoIcons.envelope_open,
+            size: 30.0,
+          ),
+          onPressed: (){},
+        ),
+            IconButton(
+          icon: const Icon(
+            CupertinoIcons.calendar,
+            size: 30.0,
+          ),
+          onPressed: (){},
+        ),
+          IconButton(
+          icon: const Icon(
+            CupertinoIcons.bell,
+            size: 30.0,
+          ),
+          onPressed: (){},
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(
+            8.0, 10.0, 20.0, 10.0),
+          child: UserProfileImage(
+          imageUrl: currentUser.imageUrl,size:36.0),
+        ),
+       ],
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          ListView(
+            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 120.0),
+            children: [
+              UpcomingRoomsList(
+                upcomingRoomsList:upcomingRoomsList),
+              const SizedBox(height: 12.0),
+              ...roomsList.map((e)=> Column(
+                children: [
+                  RoomCard(room: e),
+                ],
+              ),
+             ),
+            ],
+          ),
+          Positioned(
+            left:0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 100,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.1),
+                    Theme.of(context).scaffoldBackgroundColor,
+                  ]
+                )
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 60.0,
+            child: Container(
+              padding: const EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.circular(24.0),
+                ),
+                child: const Text.rich(TextSpan(children: [
+                  WidgetSpan(child: Icon(CupertinoIcons.add,
+                  size: 21.0,
+                  color: Colors.white,
+                  ),
+                 ),
+                 TextSpan(text: ' Start a room',
+                 style: TextStyle(color: Colors.white,
+                 fontSize: 20.0,
+                 fontWeight: FontWeight.w500,
+                 ),
+                )
+               ]
+              )
+             )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
